b847757013f4a02f64d0d7ff7b980aca18b293f3
viviane <v.buschwallace@yahoo.com>
2021-05-27 19:41:30 +0200

    returning components in App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+// Importing components
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import InputArea from "./components/InputArea";
+import ToDoList from "./components/ToDoList";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Header />
+      <InputArea />
+      <ToDoList />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
