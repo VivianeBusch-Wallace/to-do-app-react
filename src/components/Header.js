5ffc2e308ef435fe256926f0e8a318b43a125084
viviane <v.buschwallace@yahoo.com>
2021-05-31 11:18:40 +0200

    added more stuff and header

---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <div>
+      <h1>What do you plan to do today?</h1>
+    </div>
+  );
+};
+
+export default Header;
---
+++ src/components/InputArea.js
@@ -0,0 +1,8 @@
+function HandleInput() {
+  return (
+    <form>
+      <input type="text" />
+      <input type="submit" value="Save to List" />
+    </form>
+  );
+}
