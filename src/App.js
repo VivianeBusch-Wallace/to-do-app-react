ea2ffd7e8bae4ec0a5590d102f6755b177bbf13c
viviane <v.buschwallace@yahoo.com>
2021-05-31 14:19:50 +0200

    remove function in App.js and other stuff

--- src/App.js
+++ src/App.js
@@ -1,3 +1,6 @@
+// importing useState and React
+import React, { useState } from "react";
+
 // Importing components
 import Header from "./components/Header";
 import Footer from "./components/Footer";
@@ -5,6 +8,18 @@ import HandleInput from "./components/InputArea";
 import ToDoList from "./components/ToDoList";
 
 function App() {
+  // useState for the list of todos >>
+  const [list, setList] = useState([]);
+
+  // function to remove a listitem on click of trashcan (see function call in ToDoList.js) >>
+  const remove = (itemToRemove) => {
+    // << itemToRemove is the id of the clicked list item
+    // all items that do not match this id will be put into a new array which will replace the old list >>
+    const newToDoList = list.filter((item) => item.id !== itemToRemove);
+    // setList now receives the new array >>
+    setList(newToDoList);
+  };
+
   return (
     <div className="App">
       <Header />
--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from "react-dom";
 
 import App from "./App";
 
+// sass styles >>
+import "./scss/main.scss";
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
