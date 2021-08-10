a03e2b391746938c2012a65adccded41d8c6b41a
viviane <v.buschwallace@yahoo.com>
2021-08-10 14:09:20 +0200

    just added some spacing for better reading

--- src/components/InputArea.js
+++ src/components/InputArea.js
@@ -7,17 +7,19 @@ import { v4 as uuidv4 } from "uuid";
 function HandleInput(props) {
   // set useState >>
   const [userInput, setUserInput] = useState("");
+
   // target user input with useState >>
   function userInputChanges(e) {
     setUserInput(e.target.value);
   }
+
   // add the input into the object array of todos >>
   function addToList(e) {
     // prevent default of form >>
     e.preventDefault();
     //make sure the input is not processed when it's empty with an if condition >>
     if (userInput.length) {
-      // to ensure we don't replace the old listitem with the new >>
+      // create item list and to ensure we don't replace the old listitem with the new>>
       props.setList((prevState) => [
         ...prevState,
         { id: uuidv4(), title: userInput.trim(), done: false, priority: 0 },
@@ -26,6 +28,7 @@ function HandleInput(props) {
     // clear userInput on submit
     setUserInput("");
   }
+
   return (
     <form onSubmit={addToList}>
       <input type="text" value={userInput} onChange={userInputChanges} />
